7e09faa46d18f6f2c85062a89f628eb1af4d2359
lian0103 <k1319900103@gmail.com>
2020-05-11 23:33:44 +0800

    add header and navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 import $ from "jquery";
 import imgSrc from './images/food.jpg'
 
-$(document).ready(function(){
+$(document).ready(function () {
     var temp = `<h1>this is from js</h1><img src="${imgSrc}" alt="food" />`;
 })
